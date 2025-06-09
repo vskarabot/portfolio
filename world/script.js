d26848c4959b86f8fada35d315f591e2864fd87f
vasja <skarabot.vasja@gmail.com>
2025-06-09 19:48:16 +0200

    Started new mini game. Added map of the world and created basic guess all countries layout.

---
+++ world/script.js
@@ -0,0 +1,15 @@
+const visited = [
+
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const paths = document.querySelectorAll('path');
+    const euMicros = document.querySelectorAll('circle');
+
+    // contains all countries/part of countries (islands etc...)
+    const allPaths = [...paths, ...euMicros];
+
+    // when we hover country we want
+    
+});
