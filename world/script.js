b261be5983b66f3e34df95b195da0839b8df5576
vasja <skarabot.vasja@gmail.com>
2025-06-09 21:20:20 +0200

    When map is loaded, the user can now hover over countries -> this shows him all the countries teritories and displays country name.

--- world/script.js
+++ world/script.js
@@ -1,8 +1,10 @@
-const visited = [
-
-];
+// each country can have multiple teritories, so we need to mark all of it when hovering
+// in this file we listen to hovered country, find all of it's teritories and mark it on map
+// as is defined in styles.css, it transitions into new color in .2s
 
 document.addEventListener('DOMContentLoaded', () => {
+
+    const countryNameHolder = document.querySelector('.country-name');
 
     const paths = document.querySelectorAll('path');
     const euMicros = document.querySelectorAll('circle');
@@ -10,6 +12,22 @@ document.addEventListener('DOMContentLoaded', () => {
     // contains all countries/part of countries (islands etc...)
     const allPaths = [...paths, ...euMicros];
 
-    // when we hover country we want
+    allPaths.forEach(path => {
+        const allCountryTeritories = document.querySelectorAll(`[data-name="${path.dataset.name}"]`);
+        
+        path.addEventListener('mouseenter', () => {            
+            allCountryTeritories.forEach(teritory => {
+                teritory.style.fill = 'magenta';
+                countryNameHolder.textContent = path.dataset.name;
+            })
+        })
+
+        path.addEventListener('mouseleave', () => {
+            allCountryTeritories.forEach(teritory => {
+                teritory.style.fill = 'white';
+                countryNameHolder.textContent = '';
+            })
+        })
+    })
     
 });
