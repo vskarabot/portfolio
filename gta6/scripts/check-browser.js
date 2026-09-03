e8d38fa6bac05d125a9f55793836d612da61eb41
vasja <skarabot.vasja@gmail.com>
2026-09-03 10:31:27 +0200

    fix: gta vi redirects to github pages

--- gta6/scripts/check-browser.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const userAgent = navigator.userAgent;
-    const isFirefox = /Firefox/.test(userAgent);
-
-    if (isFirefox) {
-        document.getElementById('browser-warning').style.display = 'block';
-    }
-});
